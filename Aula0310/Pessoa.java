7ba9657e0b35f3652ce26b14f10bfc79ba31257b
ゆいち <victorkashima04@gmail.com>
2022-10-03 17:03:36 +0000

    up

---
+++ Aula0310/Pessoa.java
@@ -0,0 +1,8 @@
+package Aula0310;
+
+class Pessoa{
+    //Atributos
+    String name;
+    double height;
+    static String nationality;
+}
---
+++ Aula0310/PessoaExec.java
@@ -0,0 +1,13 @@
+package Aula0310;
+
+public class PessoaExec {
+    public static void main (String args[]){
+
+        //nationality está ligado à classe e não ao objeto.
+        Pessoa.nationality = "Brasileira";
+
+        System.out.println("P1: " +Pessoa.nationality);
+        System.out.println("P2: " +Pessoa.nationality);
+
+    }
+}
